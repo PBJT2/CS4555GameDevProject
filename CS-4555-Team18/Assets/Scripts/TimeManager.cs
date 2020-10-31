902d07d96e295049924e7c0e1009e1ca2af1a7b6
ptobgui300 <patriots.stars@gmail.com>
2020-10-31 16:51:48 -0700

    time system

---
+++ CS-4555-Team18/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+
+    public float startingTime;
+    private Text theText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        startingTime -= Time.deltaTime;
+        theText.text = "" + Mathf.Round (startingTime);
+
+        if(startingTime <= 0)
+        {
+            theText.text = "0";
+        }
+    }
+}
+
